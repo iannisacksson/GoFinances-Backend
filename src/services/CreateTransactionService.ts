d254bffb1c37d3da4363a22b5b4755077ad214a9
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-04-21 10:31:38 -0300

    feat: Checking account balance to make withdrawals

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,9 @@
-import { getRepository } from 'typeorm';
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface Request {
   title: string;
@@ -33,9 +34,13 @@ class CreateTransactionService {
       await categoryRepository.save(newCategory);
     }
 
-    const transactionRepository = getRepository(Transaction);
+    const transactionRepository = getCustomRepository(TransactionsRepository);
 
-    transactionRepository.find();
+    const checkBalance = await transactionRepository.getBalance();
+
+    if (value > checkBalance.total && type === 'outcome') {
+      throw new AppError('Insufficient funds');
+    }
 
     const transaction = transactionRepository.create({
       title,
