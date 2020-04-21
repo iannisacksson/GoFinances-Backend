84a8ecdd290962addfbeea7f2bad46b87e75b7ca
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-04-21 11:04:03 -0300

    feat: Transaction delete

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -4,7 +4,7 @@ import Transaction from '../models/Transaction';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -46,7 +46,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
